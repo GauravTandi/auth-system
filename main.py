84f5e6662da5e47c7cd9b86dc2aa461d1d82ff21
GauravTandi <gauravtandi10@gmail.com>
2026-06-15 20:11:46 +0530

    Implement user signup with MySql and Password hashing

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from database import Base, engine, SessionLocal
+from models import User
+from schemas import UserCreate
+from auth import hash_password
+from sqlalchemy.orm import Session
+import models
+
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
     return {"message": "Auth system running"}
+
+
+#DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/signup")
+def singup(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_pwd = hash_password(user.password)
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hashed_pwd,
+        role="user"
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User Created successfully",
+        "user_id": new_user.id
+    }
---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100))
+    email = Column(String(100), unique=True, index=True)
+    password = Column(String(255))
+    role = Column(String(50), default="user")
